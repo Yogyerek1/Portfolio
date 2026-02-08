16244c1cbe81966662bd59bba2a01e2fd5c34275
YogyerekDEV <mszebi2008@gmail.com>
2026-02-08 12:53:24 +0100

    (main): added bounce-y animation for profile img.

--- frontend/src/components/AboutMe.tsx
+++ frontend/src/components/AboutMe.tsx
@@ -12,11 +12,11 @@ export default function AboutMe({ name, description }: AboutMeProps) {
         <img
           src={profileImg}
           alt="ProfileImg"
-          className="m-16 h-32 w-32 sm:h-40 sm:w-40 md:h-48 md:w-48 lg:h-56 lg:w-56 xl:h-64 xl:w-64 rounded-full border-2 border-blue-500 shadow-[0_0_15px_5px_rgba(96,165,250,0.9),0_15px_20px_rgba(96,165,250,0.8)] hover:shadow-[0_0_30px_15px_rgba(96,165,250,1),0_20px_35px_rgba(96,165,250,0.9)] transition-all duration-300"
+          className="bounce-y m-16 h-32 w-32 sm:h-40 sm:w-40 md:h-48 md:w-48 lg:h-56 lg:w-56 xl:h-64 xl:w-64 rounded-full border-2 border-blue-500 shadow-[0_0_15px_5px_rgba(96,165,250,0.9),0_15px_20px_rgba(96,165,250,0.8)] hover:shadow-[0_0_30px_15px_rgba(96,165,250,1),0_20px_35px_rgba(96,165,250,0.9)] transition-all duration-300"
         />
       </div>
       <div>
-        <h1 className="text-3xl text-center md:text-4xl lg:text-5xl font-bold text-blue-400 drop-shadow-[0_0_15px_rgba(96,165,250,0.8)] hover:drop-shadow-[0_0_21px_rgba(96,165,250,0.85)] transition-all duration-300">
+        <h1 className="text-3xl text-center md:text-4xl lg:text-5xl font-bold text-blue-400 drop-shadow-[0_0_15px_rgba(96,165,250,0.8)] hover:drop-shadow-[0_0_19px_rgba(96,165,250,0.85)] transition-all duration-300">
           {name}
         </h1>
       </div>
