b8d097002c47c3b51735985472179baeeb1deb51
YogyerekDEV <mszebi2008@gmail.com>
2026-06-26 19:27:20 +0200

    update(main): update projects

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -43,32 +43,33 @@ export const projects: ProjectType[] = [
         technologies: ["React + TypeScript", "Express"],
     },
     {
-        title: "NexusCore",
-        description: "NexusCore is an open-source e-commerce API built with .NET 10 and PostgreSQL, designed to provide a secure, fast, and Docker-ready foundation for web stores. The project showcases a production-ready backend architecture with JWT authentication, along with the initial building blocks of a highly customizable React + TypeScript frontend.",
-        githubUrl: "https://github.com/Yogyerek1/NexusCore",
-        technologies: ["ASP NET Core", "PostgreSQL", "C#", "Typescript", "React"]
-    },
-    {
         title: "Rycloon",
         description: "A social media platform designed for gamers with tailored features, where I work as a full-stack developer, currently focusing mostly on the backend.",
         siteUrl: "https://rycloon.com",
         technologies: ["ASP NET Core", "MySQL", "Docker", "Typescript", "React", "Jenkins"]
     },
     {
+        title: "NexusCore",
+        description: "NexusCore is an open-source e-commerce API built with .NET 10 and PostgreSQL, designed to provide a secure, fast, and Docker-ready foundation for web stores. The project showcases a production-ready backend architecture with JWT authentication, along with the initial building blocks of a highly customizable React + TypeScript frontend.",
+        githubUrl: "https://github.com/Yogyerek1/NexusCore",
+        technologies: ["ASP NET Core", "PostgreSQL", "C#", "Typescript", "React"]
+    },
+    /*{
         title: "Molitio web engine",
         description: "An open-source, modular web framework where I contributed as a frontend developer. Unfortunately, the development of the project has since been discontinued, and the repository has been deleted.",
         technologies: ["React + TypeScript"],
-    },
+    },*/
     {
         title: "account-system",
         description: "A self-built account and authentication system that I developed to deeply understand and master the NestJS framework and modern backend development architectures.",
+        githubUrl: "https://github.com/Yogyerek1/account-system",
         technologies: ["Typescript", "NestJS", "PostgreSQL"],
     },
-    {
+    /*{
         title: "Hybrid engine",
         description: "A chess API built with Python and FastAPI that leverages an existing chess engine to manage and control gameplay. It acts as a lightweight middleware with clean endpoints, making it simple for anyone to develop custom client applications on top of it. The project was developed in a team for an IT competition, but we ultimately didn't enter it.",
         technologies: ["Python", "FastAPI"],
-    },
+    },*/
 ];
 
 export const SOCIAL_LINKS = {
